f4b89fd42b7bd817441182abb4fac93bc65e31c6
bahrom04 <magdiyevbahrom@gmail.com>
2026-03-10 22:18:31 +0500

    chore: update version

--- default.nix
+++ default.nix
@@ -1,70 +1,61 @@
 {
-  pkgs ? let
-    lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
-    nixpkgs = fetchTarball {
-      url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
-      sha256 = lock.narHash;
-    };
-  in
-    import nixpkgs {overlays = [];},
+  pkgs ?
+    let
+      lock = (builtins.fromJSON (builtins.readFile ./flake.lock)).nodes.nixpkgs.locked;
+      nixpkgs = fetchTarball {
+        url = "https://github.com/nixos/nixpkgs/archive/${lock.rev}.tar.gz";
+        sha256 = lock.narHash;
+      };
+    in
+    import nixpkgs { overlays = [ ]; },
   ...
-}: let
+}:
+let
   # Helpful nix function
   lib = pkgs.lib;
   # getLibFolder = pkg: "${pkg}/lib"; # uncomment for LDs
 
   manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 in
-  pkgs.rustPlatform.buildRustPackage {
-    pname = manifest.name;
-    version = manifest.version;
+pkgs.rustPlatform.buildRustPackage {
+  pname = manifest.name;
+  version = manifest.version;
 
-    # Your govnocodes
-    src = pkgs.lib.cleanSource ./.;
+  # Your govnocodes
+  src = pkgs.lib.cleanSource ./.;
 
-    cargoLock = {
-      lockFile = ./Cargo.lock;
-      # Use this if you have dependencies from git instead
-      # of crates.io in your Cargo.toml
-      # outputHashes = {
-      #   # Sha256 of the git repository, doesn't matter if it's monorepo
-      #   "example-0.1.0" = "sha256-80EwvwMPY+rYyti8DMG4hGEpz/8Pya5TGjsbOBF0P0c=";
-      # };
-    };
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    # Use this if you have dependencies from git instead
+    # of crates.io in your Cargo.toml
+    # outputHashes = {
+    #   # Sha256 of the git repository, doesn't matter if it's monorepo
+    #   "example-0.1.0" = "sha256-80EwvwMPY+rYyti8DMG4hGEpz/8Pya5TGjsbOBF0P0c=";
+    # };
+  };
 
-    # Compile time dependencies
-    nativeBuildInputs = with pkgs; [
-      # Nix
-      nixd
-      statix
-      deadnix
-      alejandra
+  # Compile time dependencies
+  nativeBuildInputs = with pkgs; [
+    # Rust
+    rustc
+    cargo
+  ];
 
-      # Rust
-      rustc
-      cargo
-      rustfmt
-      clippy
-      rust-analyzer
-      cargo-watch
-      cargo-expand
-    ];
+  buildInputs = [
+    pkgs.rustc
+    pkgs.cargo
+  ];
 
-    buildInputs = [
-      pkgs.rustc
-      pkgs.cargo
-    ];
+  # Set Environment Variables
+  RUST_BACKTRACE = 1;
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
-    # Set Environment Variables
-    RUST_BACKTRACE = 1;
-    RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-
-    meta = with lib; {
-      homepage = manifest.homepage;
-      description = manifest.description;
-      # https://github.com/NixOS/nixpkgs/blob/master/lib/licenses.nix
-      license = with licenses; [mit];
-      platforms = with platforms; linux ++ darwin;
-      maintainers = with maintainers; [bahrom04];
-    };
-  }
+  meta = with lib; {
+    homepage = manifest.homepage;
+    description = manifest.description;
+    # https://github.com/NixOS/nixpkgs/blob/master/lib/licenses.nix
+    license = with licenses; [ mit ];
+    platforms = platforms.linux;
+    maintainers = with maintainers; [ bahrom04 ];
+  };
+}
