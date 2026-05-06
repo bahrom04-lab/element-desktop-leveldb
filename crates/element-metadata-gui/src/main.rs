a93405361e6750d5034f824af7cd4317c5a0811f
bahrom04 <magdiyevbahrom@gmail.com>
2026-05-06 13:42:54 +0500

    chore: added app_id

--- crates/element-metadata-gui/src/main.rs
+++ crates/element-metadata-gui/src/main.rs
@@ -2,6 +2,6 @@ use element_metadata_gui::window::App;
 use relm4::RelmApp;
 
 fn main() {
-    let app = RelmApp::new("relm4.example.components");
+    let app = RelmApp::new("uz.bahrom04.element-desktop-leveldb");
     app.run::<App>(());
 }
